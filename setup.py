5419fa5e74cc6aea9afc787c84ac7232082fd73b
Swapnil Singh <drswapnil98@gmail.com>
2025-01-30 00:21:34 +0530

    folder structure edit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup (
+    name = 'Movie Recommender System',
+    version= '0.0.0',
+    author= 'Swapnil Singh',
+    author_email= 'drswapnil98@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
